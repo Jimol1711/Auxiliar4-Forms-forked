9f4bd2f809c24262a838ea8ca5fe82f0c1c7f800
Jimol1711 <juan.molina.l@ug.uchile.cl>
2024-04-29 22:04:34 -0400

    aux4 finished

--- todoapp/views.py
+++ todoapp/views.py
@@ -6,6 +6,9 @@ from todoapp.models import Tarea
 from categorias.models import Categoria
 
 from todoapp.models import User
+from todoapp.forms import NuevaTareaForm
+from todoapp.forms import NuevaTareaModelForm
+
 def tareas(request):  # the index view
     if request.user.is_authenticated:
         mis_tareas = Tarea.objects.filter(owner=request.user)# quering all todos with the object manager
@@ -14,20 +17,18 @@ def tareas(request):  # the index view
     categorias = Categoria.objects.all()  # getting all categories with object manager
 
     if request.method == "GET":
-        return render(request, "todoapp/index.html", {"tareas": mis_tareas, "categorias": categorias})
+        form_tarea = NuevaTareaModelForm()
+        return render(request, "todoapp/index.html", {"tareas": mis_tareas, "form_tarea":form_tarea})
+
 
     if request.method == "POST":  # revisar si el método de la request es POST
-        if "taskAdd" in request.POST:  # verificar si la request es para agregar una tarea (esto está definido en el button)
-            titulo = request.POST["titulo"]  # titulo de la tarea
-            nombre_categoria = request.POST["selector_categoria"]  # nombre de la categoria
-            categoria = Categoria.objects.get(nombre=nombre_categoria)  # buscar la categoría en la base de datos
-            contenido = request.POST["contenido"]  # contenido de la tarea
+        form_tarea= NuevaTareaModelForm(request.POST)
+        if form_tarea.is_valid():
+            nueva_tarea = form_tarea.save() # save() de ModelForm
             if request.user.is_authenticated:
-                nueva_tarea = Tarea(titulo=titulo, contenido=contenido, categoria=categoria,owner=request.user)  # Crear la tarea
-            else:
-                nueva_tarea = Tarea(titulo=titulo, contenido=contenido, categoria=categoria)
-            nueva_tarea.save()  # guardar la tarea en la base de datos.
-            return redirect("/tareas")  # recargar la página.
+                nueva_tarea.owner = request.user
+                nueva_tarea.save()  # save() de Model
+        return render(request, "todoapp/index.html", {"tareas": mis_tareas, "form_tarea": form_tarea})
 
 
 from django.http import HttpResponseRedirect
@@ -70,3 +71,4 @@ def login_user(request):
 def logout_user(request):
    logout(request)
    return HttpResponseRedirect('/tareas')
+
